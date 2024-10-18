4893a5340b1ee871bfba9c63045ad55a1d8e563a
robertinws <bertinws@gmail.com>
2024-10-17 19:57:16 -0700

    Adição do id para a view menu no design e animação de troca de tela

--- app/src/main/java/com/example/prova29/TelaMaterias.java
+++ app/src/main/java/com/example/prova29/TelaMaterias.java
@@ -1,24 +1,49 @@
 package com.example.prova29;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.prova29.databinding.ActivityTelaMateriasBinding;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class TelaMaterias extends AppCompatActivity {
+
+    private ActivityTelaMateriasBinding view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_tela_materias);
+        view = ActivityTelaMateriasBinding.inflate(getLayoutInflater());
+        setContentView(view.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        view.recyclerView.setLayoutManager(new LinearLayoutManager(TelaMaterias.this));
+        view.recyclerView.setHasFixedSize(true);
+
+        Animation animation = new AlphaAnimation(1, 0);
+        animation.setDuration(2000);
+
+        view.viewHarmonico.setAnimation(animation);
+        view.viewHarmonico.setVisibility(View.GONE);
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("http://10.0.2.2:3000").addConverterFactory(GsonConverterFactory.create()).build();
+
     }
 }
