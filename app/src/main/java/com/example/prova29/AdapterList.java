8e8134cd56d1950892f1b93c5685c6cf63b80950
robertinws <bertinws@gmail.com>
2024-10-17 20:03:28 -0700

    Class do adapter da lista feita e listagem

---
+++ app/src/main/java/com/example/prova29/AdapterList.java
@@ -0,0 +1,53 @@
+package com.example.prova29;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdapterList extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private List<Disciplinas> list = new ArrayList<>();
+    private Context context;
+
+    public AdapterList(List<Disciplinas> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        Disciplinas dis = list.get(position);
+
+        TextView txtMateria = holder.itemView.findViewById(R.id.txtMateria);
+
+        txtMateria.setText(dis.getNome_disciplina());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+}
--- app/src/main/java/com/example/prova29/TelaMaterias.java
+++ app/src/main/java/com/example/prova29/TelaMaterias.java
@@ -1,10 +1,13 @@
 package com.example.prova29;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,12 +18,28 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.prova29.databinding.ActivityTelaMateriasBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public class TelaMaterias extends AppCompatActivity {
 
     private ActivityTelaMateriasBinding view;
+    private List<Disciplinas> list = new ArrayList<>();
+
+    private interface Lista {
+
+        @GET("/disciplinas-usuario/{id}")
+        Call<List<Disciplinas>> listar(@Path("id") int id);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +62,45 @@ public class TelaMaterias extends AppCompatActivity {
         view.viewHarmonico.setAnimation(animation);
         view.viewHarmonico.setVisibility(View.GONE);
 
+
+
         Retrofit retrofit = new Retrofit.Builder().baseUrl("http://10.0.2.2:3000").addConverterFactory(GsonConverterFactory.create()).build();
+
+        Lista lista = retrofit.create(Lista.class);
+
+        SharedPreferences cache = getSharedPreferences("login", MODE_PRIVATE);
+
+        int id = cache.getInt("id", 0);
+
+        lista.listar(id).enqueue(new Callback<List<Disciplinas>>() {
+            @Override
+            public void onResponse(Call<List<Disciplinas>> call, Response<List<Disciplinas>> response) {
+
+                if (response.isSuccessful()) {
+
+                    list = response.body();
+                    AdapterList adapterList = new AdapterList(list, TelaMaterias.this);
+                    view.recyclerView.setAdapter(adapterList);
+
+                } else if (response.code() == 404){
+
+                    Toast.makeText(TelaMaterias.this, "Curso não encontrado", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    Log.e("ERROR", "Error: " + response.code());
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Disciplinas>> call, Throwable throwable) {
+
+                Log.e("ERROR", "Error: " + throwable.getMessage());
+
+            }
+        });
 
     }
 }
